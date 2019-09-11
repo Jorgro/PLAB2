0b5ab45f3a400ce528ff575d742fc79d5b9fd2e7
jorgro <joergen.rosager@gmail.com>
2019-09-11 10:33:38 +0200

    Renamed

---
+++ Project3/Person.py
@@ -0,0 +1,18 @@
+
+#En som mottar/sender/hacker meldinger
+class Person:
+
+    def __init__(self, key):
+        #Nøkkelen: 
+        self.key = key
+    
+
+
+    def set_key(self):
+        return
+    
+    def get_key(self):
+        return
+    
+    
+
